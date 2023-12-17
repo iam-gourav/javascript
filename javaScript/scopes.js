895077a96b3b898aee3ad54e8fe6f84b58b60361
iam-gourav <140313751+iam-gourav@users.noreply.github.com>
2023-12-17 17:27:29 +0530

    scopes

---
+++ javaScript/scopes.js
@@ -0,0 +1,15 @@
+// scopes in js 
+//global scope
+let a = 300
+{
+    // block scope 
+    let a = 10;
+    const b = 20;
+    var c = 30;
+    console.log(a)
+}
+
+
+console.log(a)
+// console.log(b)
+console.log(c)
